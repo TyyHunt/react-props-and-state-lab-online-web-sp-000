8a48498d1c23e495b73f55e0d48d595c0f5cf960
Tyler Hunt <tyhunt04@gmail.com>
2019-07-30 19:39:12 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,29 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (type) => {
+    this.setState({ filters: { type: type } });
+  }
+
+  onFindPetsClick = () => {
+    let url = "/api/pets"
+    if (this.state.filters.type !== 'all') {
+      url += "?type=" + this.state.filters.type
+    }
+    fetch(url)
+      .then(response => response.json())
+      .then(json => this.setState({pets: json}));
+  }
+
+  onAdoptPet = (petID) => {
+    let updatedPets = this.state.pets
+    let petIndex = this.state.pets.findIndex(pet => pet.id === petID)
+    updatedPets[petIndex].isAdopted = true;
+    this.setState({
+      pets: updatedPets
+    });
+  }
+
   render() {
     return (
       <div className="ui container">
